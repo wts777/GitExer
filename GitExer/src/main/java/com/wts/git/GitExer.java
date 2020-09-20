626f2aa35a685cafc14e1dc5b4add3b0f83dcf26
wts777 <1456875605@qq.com>
2020-09-20 18:56:10 +0800

    Clone成功

--- GitExer/src/main/java/com/wts/git/GitExer.java
+++ GitExer/src/main/java/com/wts/git/GitExer.java
@@ -4,5 +4,6 @@ public class GitExer {
     public static void main(String[] args) {
         System.out.println("Hello Git");
         System.out.println("大家好");
+        System.out.println("克隆成功");
     }
 }
